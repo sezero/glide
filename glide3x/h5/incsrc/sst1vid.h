b17cdee7441c489d878ba2597d5f1defee506f59
alanh <>
2000-11-16 19:04:35 +0000

    merge trunk into 64bit branch.

--- glide3x/h5/incsrc/sst1vid.h
+++ glide3x/h5/incsrc/sst1vid.h
@@ -19,6 +19,8 @@
 **
 ** $Header$
 ** $Log: 
+**  7    3dfx      1.4.1.0.1.0 10/11/00 Brent           Forced check in to enforce
+**       branching.
 **  6    3dfx      1.4.1.0     06/20/00 Joseph Kain     Changes to support the
 **       Napalm Glide open source release.  Changes include cleaned up offensive
 **       comments and new legal headers.
