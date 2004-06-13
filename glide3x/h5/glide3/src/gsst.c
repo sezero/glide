1bb2e0b0d20c96374d196256e57fd50f14b7e21f
koolsmoky <>
2004-06-13 08:26:28 +0000

    fill in TAA jitter values for 4-chip 8xFSAA and 1-chip 2xFSAA

--- glide3x/h5/glide3/src/gsst.c
+++ glide3x/h5/glide3/src/gsst.c
@@ -2055,6 +2055,40 @@ GR_EXT_ENTRY(grSstWinOpenExt, GrContext_t, ( FxU32                   hWnd,
       /* TODO: temporalAA over 4-chip 8xFSAA
        * probably too slow and probably not needed anyway
        */
+      /* use 8xFSAA jitter values for now */
+      _GlideRoot.environment.aaXOffset[13][0] =
+      _GlideRoot.environment.aaXOffset[14][0] = _GlideRoot.environment.aaXOffset[12][0];
+      _GlideRoot.environment.aaXOffset[13][1] =
+      _GlideRoot.environment.aaXOffset[14][1] = _GlideRoot.environment.aaXOffset[12][1];
+      _GlideRoot.environment.aaXOffset[13][2] =
+      _GlideRoot.environment.aaXOffset[14][2] = _GlideRoot.environment.aaXOffset[12][2];
+      _GlideRoot.environment.aaXOffset[13][3] =
+      _GlideRoot.environment.aaXOffset[14][3] = _GlideRoot.environment.aaXOffset[12][3];
+      _GlideRoot.environment.aaXOffset[13][4] =
+      _GlideRoot.environment.aaXOffset[14][4] = _GlideRoot.environment.aaXOffset[12][4];
+      _GlideRoot.environment.aaXOffset[13][5] =
+      _GlideRoot.environment.aaXOffset[14][5] = _GlideRoot.environment.aaXOffset[12][5];
+      _GlideRoot.environment.aaXOffset[13][6] =
+      _GlideRoot.environment.aaXOffset[14][6] = _GlideRoot.environment.aaXOffset[12][6];
+      _GlideRoot.environment.aaXOffset[13][7] =
+      _GlideRoot.environment.aaXOffset[14][7] = _GlideRoot.environment.aaXOffset[12][7];
+      
+      _GlideRoot.environment.aaYOffset[13][0] =
+      _GlideRoot.environment.aaYOffset[14][0] = _GlideRoot.environment.aaYOffset[12][0];
+      _GlideRoot.environment.aaYOffset[13][1] =
+      _GlideRoot.environment.aaYOffset[14][1] = _GlideRoot.environment.aaYOffset[12][1];
+      _GlideRoot.environment.aaYOffset[13][2] =
+      _GlideRoot.environment.aaYOffset[14][2] = _GlideRoot.environment.aaYOffset[12][2];
+      _GlideRoot.environment.aaYOffset[13][3] =
+      _GlideRoot.environment.aaYOffset[14][3] = _GlideRoot.environment.aaYOffset[12][3];
+      _GlideRoot.environment.aaYOffset[13][4] =
+      _GlideRoot.environment.aaYOffset[14][4] = _GlideRoot.environment.aaYOffset[12][4];
+      _GlideRoot.environment.aaYOffset[13][5] =
+      _GlideRoot.environment.aaYOffset[14][5] = _GlideRoot.environment.aaYOffset[12][5];
+      _GlideRoot.environment.aaYOffset[13][6] =
+      _GlideRoot.environment.aaYOffset[14][6] = _GlideRoot.environment.aaYOffset[12][6];
+      _GlideRoot.environment.aaYOffset[13][7] =
+      _GlideRoot.environment.aaYOffset[14][7] = _GlideRoot.environment.aaYOffset[12][7];
     }
     else if (gc->chipCount == 2 && gc->grPixelSample == 2) {
       _GlideRoot.environment.aaXOffset[13][0] = _GlideRoot.environment.aaXOffset[6][0];
@@ -2090,6 +2124,15 @@ GR_EXT_ENTRY(grSstWinOpenExt, GrContext_t, ( FxU32                   hWnd,
       /* TODO: temporalAA over 1-chip 2xFSAA
        * probably too slow
        */
+      _GlideRoot.environment.aaXOffset[13][0] = _GlideRoot.environment.aaXOffset[6][0];
+      _GlideRoot.environment.aaXOffset[13][1] = _GlideRoot.environment.aaXOffset[6][2];
+      _GlideRoot.environment.aaYOffset[13][0] = _GlideRoot.environment.aaYOffset[6][0];
+      _GlideRoot.environment.aaYOffset[13][1] = _GlideRoot.environment.aaYOffset[6][2];
+      
+      _GlideRoot.environment.aaXOffset[14][0] = _GlideRoot.environment.aaXOffset[6][1];
+      _GlideRoot.environment.aaXOffset[14][1] = _GlideRoot.environment.aaXOffset[6][3];
+      _GlideRoot.environment.aaYOffset[14][0] = _GlideRoot.environment.aaYOffset[6][1];
+      _GlideRoot.environment.aaYOffset[14][1] = _GlideRoot.environment.aaYOffset[6][3];
     }
     if (gc->grPixelSample < 2) {
       int i;
