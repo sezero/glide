97fbce5c9cf0b8e523ed76c5d207f5fb618b9270
koolsmoky <>
2004-12-12 15:29:44 +0000

    cosmetics

--- glide2x/cvg/glide/src/rcver.h
+++ glide2x/cvg/glide/src/rcver.h
@@ -2,6 +2,8 @@
 #define MANVERSION              2
 #define MANREVISION             53
 
+#define BUILD_NUMBER            0x666
+
 #ifndef GLIDE3
 #define VERSIONSTR "2.53\0"
 #else
@@ -9,11 +11,11 @@
 #endif
 
 #if defined(CVG) || defined(VOODOO2)
-#   define HWSTR   " Voodoo(tm)\0"
+#   define HWSTR   " Voodoo^2(tm)\0"
 #   ifdef NT_BUILD
-#     define PRODNAME "Glide(tm) for Voodoo\251 and Windows\256 NT\0"
+#     define PRODNAME "Glide(tm) for Voodoo^2\251 and Windows\256 NT\0"
 #   else
-#     define PRODNAME "Glide(tm) for Voodoo\251 and Windows\256 95/98\0"
+#     define PRODNAME "Glide(tm) for Voodoo^2\251 and Windows\256 95/98\0"
 #   endif /* NT_BUILD */
 #elif defined(H3)
 #   define HWSTR   " Banshee(tm)\0"
