d257dd2bc287827155cf14bf92f9f87aa3fcd267
sezero <sezero@users.sourceforge.net>
2018-08-03 18:43:33 +0300

    h3cinit.c: removed an ugly hack for building under dos.

--- glide3x/h5/cinit/h3cinit.c
+++ glide3x/h5/cinit/h3cinit.c
@@ -333,12 +333,7 @@
 #include <h3cinit.h>
 #include <h3.h>
 
-/* 8.3 fun */
-#ifdef __DOS32__
-#include "h3cini~1.h"
-#else
 #include "h3cinitdd.h"
-#endif
 
 // Fixing support for H4 pll tables.
 #ifdef H4
