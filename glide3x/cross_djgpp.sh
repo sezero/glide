fe35279d12168a676528e7d538d944dac6aff8f9
Ozkan Sezer <sezeroz@gmail.com>
2020-07-08 15:10:00 +0300

    updated djgpp cross-build scripts

--- glide3x/cross_djgpp.sh
+++ glide3x/cross_djgpp.sh
@@ -1,6 +1,11 @@
 #! /bin/sh
 
-# make sure the DJGPP toolchain is on your PATH
+# update the following to reflect your own cross-toolchain
+PATH=/usr/local/cross-djgpp/bin:$PATH
+DXE_LD_LIBRARY_PATH=/usr/local/cross-djgpp/i586-pc-msdosdjgpp/lib
+
+export PATH
+export DXE_LD_LIBRARY_PATH
 
 test -d swlibs || ln -s ../swlibs swlibs
 make -f Makefile.DJ FX_GLIDE_HW=cvg clean
@@ -13,4 +18,3 @@ make -f Makefile.DJ FX_GLIDE_HW=sst1 clean
 make -f Makefile.DJ FX_GLIDE_HW=sst1 USE_X86=1 USE_MMX=1 USE_3DNOW=1 USE_SSE=1 USE_SSE2=1 CC=i586-pc-msdosdjgpp-gcc AR=i586-pc-msdosdjgpp-ar
 make -f Makefile.DJ FX_GLIDE_HW=sst96 clean
 make -f Makefile.DJ FX_GLIDE_HW=sst96 USE_X86=1 USE_MMX=1 USE_3DNOW=1 USE_SSE=1 USE_SSE2=1 CC=i586-pc-msdosdjgpp-gcc AR=i586-pc-msdosdjgpp-ar
-
